770a3a6b5e82f85b2855f2c831e93be08e619256
hyumilis <102694635+hyumilis@users.noreply.github.com>
2025-11-03 17:42:14 +0100

    completed Main implementaition to current state

--- Program/Main.ts
+++ Program/Main.ts
@@ -1,8 +1,13 @@
 namespace SolarSystem {
     window.addEventListener("load", handleLoad);
     export let crc2: CanvasRenderingContext2D;
-    let actors: Actor[];
+    const actors: Actor[] = [];
     let timescale: number;
+    const clickedPos: Vector2 = new Vector2(0, 0);
+    let infobox: Infobox;
+    let clicked: boolean = false;
+    let delaytimescale: number = 1;
+
 
 
     function handleLoad(_event: Event): void {
@@ -12,6 +17,23 @@ namespace SolarSystem {
         }
         crc2 = canvas.getContext("2d") as CanvasRenderingContext2D;
         crc2.fillStyle = "black";
+
+        canvas.addEventListener("mousedown", hndlclick);
+        window.setInterval(update, 20);
     }
 
+    function update() {
+        let deltaTime = getDeltaTime();
+        for (let actor of actors) {
+            actor.process(delaytimescale);
+            if(clicked){
+                actor.isclicked(clickedPosq);
+            }
+        }
+    }
+
+    function hndlclick(_event: MouseEvent): void {
+        clickedPos.set(_event.clientX - crc2.canvas.offsetLeft, _event.clientY - crc2.canvas.offsetTop);
+        clicked = true;
+    }
 }
