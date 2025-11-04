ca701cba357c14005fae47a7bc602bbc321d3594
hyumilis <102694635+hyumilis@users.noreply.github.com>
2025-11-04 14:31:25 +0100

    removed matrix from celestialBody

--- Program/CelestialBody.ts
+++ Program/CelestialBody.ts
@@ -1,14 +1,13 @@
 namespace SolarSystem {
     export class CelestialBody extends Actor {
-
         public size: number;
         public orbitalPeriod: number;
         public rotationalSpeed: number;
         public info: Info;
 
 
-        public constructor(_name: string, _size: number, _orbitalPeriod: number, _matrices: Matrix2) {
-            super(_name, _matrices);
+        public constructor(_name: string, _size: number, _orbitalPeriod: number) {
+            super(_name,);
         }
 
         public process(): void {
