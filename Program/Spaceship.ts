a91e32ca68e2ee33b59479eb694403586c3d2644
Dimenzio6066 <lukasniemann05@gmail.com>
2025-11-04 17:00:57 +0100

    added draw function to spaceship

--- Program/Spaceship.ts
+++ Program/Spaceship.ts
@@ -1,16 +1,18 @@
 namespace SolarSystem {
     export class Spaceship extends Actor {
-        public destination: Vector2;
+        public destination?: CelestialBody;
         public info: Info;
+        private rotation: number;
 
         public constructor(_name: string, _position: Vector2, _matrices: Matrix2[]) {
             super(_name, _matrices);
             this.path = this.createShipPath();
 
         }
-        public determineDestination(): void {
-
+        public determineDestination(_destination: CelestialBody): void {
+            this.destination = _destination;
         }
+        
         public process(): void {
             this.move();
             this.draw();
@@ -20,7 +22,12 @@ namespace SolarSystem {
 
         }
         public draw(): void {
-
+            crc2.save();
+            crc2.translate(this.positionOffset.x, this.positionOffset.y);
+            crc2.rotate(this.rotation);
+            this.transform = crc2.getTransform();
+            crc2.stroke(path);
+            crc2.restore();
         }
 
         private createShipPath(): Path2D {
