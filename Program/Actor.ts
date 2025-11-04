f3b35313292389a9b80a7424f289d7007a39fb4e
Dimenzio6066 <lukasniemann05@gmail.com>
2025-11-04 14:15:50 +0100

    Modified Actor

--- Program/Actor.ts
+++ Program/Actor.ts
@@ -1,26 +1,30 @@
 namespace SolarSystem {
-    export class Actor {
+    export abstract class Actor {
         public name: string;
-        public globalPosition: Vector2;
-        public info: Info;
-        public path: Path2D;
+        public transform!: DOMMatrix;
+        // public globalPosition: Vector2;
+        public info!: Info;
+       
 
 
 
-        public constructor(_name: string, _matrices?: Matrix2[]) {
+        public constructor(_name: string) {
             this.name = _name;
-            this.matrices = _matrices;
+            
 
         }
 
         public isclicked(_clickedPos: Vector2): boolean {
-            const global: DOMPoint = new DOMPoint(_event.offsetX, _event.offsetY);
+            const global: DOMPoint = new DOMPoint(_clickedPos.x, _clickedPos.y);
             console.log("Global:", global);
             for (const actor of actors) {
                 const inverse: DOMMatrix = actor.transform!.inverse();
                 const local: DOMPoint = inverse.transformPoint(global);
-                const hit: boolean = crc2.isPointInPath(actor.path, local.x, local.y);
-                if (hit) {
+                const localVector2: Vector2 = new Vector2(local.x,local.y);
+
+
+            
+                if () {
                     return true;
                 }
             }
